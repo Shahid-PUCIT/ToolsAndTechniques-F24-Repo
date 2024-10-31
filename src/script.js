1ecb9c876ebdff254be7e5ed7b63147bf3c5d0cf
M-Muddasir <chmudasirbwn775@gmail.com>
2024-11-01 00:55:16 +0500

    refactor:script file formatted

--- src/script.js
+++ src/script.js
@@ -1,12 +1,13 @@
 // Sticky Navbar
 window.addEventListener("scroll", function () {
-    let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-  });
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
-
-  // update current year
-window.onload = function() {
+// update current year
+window.onload = function () {
   const currentYearEl = document.querySelector("#currentYear");
-  if(currentYearEl) {currentYearEl.innerText = new Date().getFullYear();}
-}
+  if (currentYearEl) {
+    currentYearEl.innerText = new Date().getFullYear();
+  }
+};
